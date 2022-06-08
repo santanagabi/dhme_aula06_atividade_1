d60ae27f4b8bcd618feb4b801059fd1efb996509
CassioFreitas <cgf04df@gmail.com>
2022-06-08 17:26:59 -0300

    Upload de pasta com solucao do projeto para cassiofreitas

---
+++ dhme_aula06_atividade_1_cassio/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.Write("Qual seu nome? ");
+string nomeDoUsuario = Console.ReadLine();
+Console.Write("Qual sua idade? ");
+int idadeDoUsuario = Convert.ToInt32(Console.ReadLine());
+Console.Write("Qual o ano atual? ");
+int anoAtual = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Olá " + nomeDoUsuario + ", você nasceu em " + (anoAtual - idadeDoUsuario));
